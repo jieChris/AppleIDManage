08b80f1cd9dbb11c8986b5a2590752fc827c02c1
jieChris <1203214493@qq.com>
2026-08-12 03:03:22 +0800

    ops: package shared vault deployment

---
+++ backup_vault.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+DOCKER=/usr/bin/docker
+CONTAINER=appleid-api
+
+"$DOCKER" exec --user 0 "$CONTAINER" /usr/local/bin/python /app/vault_api.py \
+  --backup \
+  --db /data/vault.db \
+  --key /run/secrets/vault.key \
+  --backup-root /backups
